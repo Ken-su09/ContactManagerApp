85b83bfa208838d91251331a1771032e59f362e0
Ken Suon <ken.su_dev@hotmail.com>
2021-09-16 17:27:15 +0200

    Setup Dependency Injection with Hilt

---
+++ app/src/main/java/com/suonk/contactmanagerapp/hilt/AppModule.kt
@@ -0,0 +1,23 @@
+package com.suonk.contactmanagerapp.hilt
+
+import android.content.Context
+import androidx.room.Room
+import com.suonk.contactmanagerapp.models.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context, AppDatabase::class.java, "contact_db"
+    ).build()
+
+    @Provides
+    fun provideDao(database: AppDatabase) = database.contactDao()
+}
