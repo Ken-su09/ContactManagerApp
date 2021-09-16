e60e7196417bf63f6e3f72cdecd98983c81c74d9
Ken Suon <ken.su_dev@hotmail.com>
2021-09-16 17:28:18 +0200

    Setup MVVM Pattern

---
+++ app/src/main/java/com/suonk/contactmanagerapp/repositories/ContactManagerRepository.kt
@@ -0,0 +1,31 @@
+package com.suonk.contactmanagerapp.repositories
+
+import androidx.annotation.WorkerThread
+import com.suonk.contactmanagerapp.models.dao.ContactDao
+import com.suonk.contactmanagerapp.models.data.Contact
+import javax.inject.Inject
+
+class ContactManagerRepository @Inject constructor(private val dao: ContactDao) {
+
+    val allContactsAlphabet = dao.getAllContactsOrderByAlphabet()
+    val allContactsInverseAlphabet = dao.getAllContactsOrderByInverseAlphabet()
+
+    fun getContactById(contactId: Int) = dao.getContactById(contactId)
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun addNewContact(contact: Contact) {
+        dao.addNewContact(contact)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun updateContact(contact: Contact) {
+        dao.updateContact(contact)
+    }
+
+    @WorkerThread
+    suspend fun deleteContact(contact: Contact) {
+        dao.deleteContact(contact)
+    }
+}
---
+++ app/src/main/java/com/suonk/contactmanagerapp/viewmodels/ContactManagerViewModel.kt
@@ -0,0 +1,32 @@
+package com.suonk.contactmanagerapp.viewmodels
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.suonk.contactmanagerapp.models.data.Contact
+import com.suonk.contactmanagerapp.repositories.ContactManagerRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ContactManagerViewModel @Inject constructor(private val repository: ContactManagerRepository) :
+    ViewModel() {
+
+    val allContactsAlphabet = repository.allContactsAlphabet
+    val allContactsInverseAlphabet = repository.allContactsInverseAlphabet
+
+    fun getContactById(contactId: Int) = repository.getContactById(contactId)
+
+    fun addNewContact(contact: Contact) = viewModelScope.launch {
+        repository.addNewContact(contact)
+    }
+
+    suspend fun updateContact(contact: Contact) = viewModelScope.launch {
+        repository.updateContact(contact)
+    }
+
+    suspend fun deleteContact(contact: Contact) = viewModelScope.launch {
+        repository.deleteContact(contact)
+    }
+}
