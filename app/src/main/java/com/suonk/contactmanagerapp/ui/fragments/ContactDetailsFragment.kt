a27749adb964609a50e6ebcbbc5cb6441c1408a2
Ken Suon <ken.su_dev@hotmail.com>
2021-09-17 23:26:40 +0200

    Work on ContactDetails UI

--- app/src/main/java/com/suonk/contactmanagerapp/ui/fragments/ContactDetailsFragment.kt
+++ app/src/main/java/com/suonk/contactmanagerapp/ui/fragments/ContactDetailsFragment.kt
@@ -13,10 +13,6 @@ class ContactDetailsFragment : Fragment() {
 
     private var binding: FragmentContactDetailBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
