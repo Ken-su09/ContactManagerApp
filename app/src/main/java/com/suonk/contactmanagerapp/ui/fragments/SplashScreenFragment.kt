b045fad625fc9c2dc30049a02f227202e822ddf3
Ken Suon <ken.su_dev@hotmail.com>
2021-09-17 16:45:35 +0200

    Add a SplashScreenAnimation

--- app/src/main/java/com/suonk/contactmanagerapp/ui/fragments/SplashScreenFragment.kt
+++ app/src/main/java/com/suonk/contactmanagerapp/ui/fragments/SplashScreenFragment.kt
@@ -1,10 +1,14 @@
 package com.suonk.contactmanagerapp.ui.fragments
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import com.suonk.contactmanagerapp.R
 import com.suonk.contactmanagerapp.databinding.FragmentSplashScreenBinding
 
@@ -21,7 +25,14 @@ class SplashScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        appLogoAnimation()
         return binding!!.root
+    }
+
+    private fun appLogoAnimation() {
+        val animation = AnimationUtils.loadAnimation(context, R.anim.suddenly_appear)
+        binding!!.appLogo.startAnimation(animation)
+        binding!!.appName.startAnimation(animation)
     }
 
     override fun onDestroyView() {
