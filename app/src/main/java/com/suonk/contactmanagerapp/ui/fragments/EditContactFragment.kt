2f7fbcbd742a7e5193a94d8a1f5a778350e6055e
Ken Suon <ken.su_dev@hotmail.com>
2021-09-18 15:35:41 +0200

    Work on EditContact UI

---
+++ app/src/main/java/com/suonk/contactmanagerapp/ui/fragments/EditContactFragment.kt
@@ -0,0 +1,45 @@
+package com.suonk.contactmanagerapp.ui.fragments
+
+import android.content.res.Resources
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.graphics.drawable.toDrawable
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.suonk.contactmanagerapp.databinding.FragmentEditContactBinding
+import com.suonk.contactmanagerapp.viewmodels.ContactManagerViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class EditContactFragment : Fragment() {
+
+    private var binding: FragmentEditContactBinding? = null
+    private val viewModel: ContactManagerViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentEditContactBinding.inflate(inflater, container, false)
+        initializeUI()
+        return binding!!.root
+    }
+
+    private fun initializeUI() {
+        getContactFromViewModel()
+    }
+
+    private fun getContactFromViewModel() {
+        viewModel.contactLiveData.observe(viewLifecycleOwner, { contact ->
+            binding!!.userImage.setImageDrawable(contact.img!!.toDrawable(Resources.getSystem()))
+            binding!!.userName.text = "${contact.firstName} ${contact.lastName}"
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
